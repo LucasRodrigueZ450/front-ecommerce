c5dabbc9d2476e3b49b6ceecc4eb1cc3878dab15
Lucas Rodrigues <65141835+LucasRodrigueZ450@users.noreply.github.com>
2025-11-29 20:15:19 -0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-import "./register.css";
+import "./Register.css";
 
 function Register() {
   const { register, error, loading } = useAuth();
